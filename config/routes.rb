40b78079285f4582dff7990ce8260bdd0f9b07f3
Seth Taylor <sethtaylor26@gmail.com>
2014-09-23 14:14:14 -0700

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'people' => 'people#all'
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'people/:id' => 'people#person'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
